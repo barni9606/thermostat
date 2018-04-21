2f9fdd006761edc858512952ddf3353e25d82da3
barni9606 <schenkbarnabas@gmail.com>
2018-04-21 21:01:36 +0200

    regulator GPIO cleanup

--- BackEnd/regulator.py
+++ BackEnd/regulator.py
@@ -15,6 +15,8 @@ week = None
 lock = Lock()
 # w1Loc = '/sys/bus/w1/devices/28-5ec67b126461'
 w1Loc = '.'
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio_number, GPIO.OUT)
 
 
 def getTempC():
@@ -23,39 +25,33 @@ def getTempC():
 
 
 def activate_relay():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(gpio_number, GPIO.OUT)
     GPIO.output(gpio_number, GPIO.HIGH)
-    GPIO.cleanup()
 
 
 def deactivate_relay():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(gpio_number, GPIO.OUT)
     GPIO.output(gpio_number, GPIO.LOW)
-    GPIO.cleanup()
 
 
 class Regulator(Thread):
 
     def run(self):
         while True:
+            day = datetime.today().weekday()
+            now = datetime.now().time()
+            period = None
             with lock:
-                day = datetime.today().weekday()
-                now = datetime.now().time()
-                period = None
                 for i in week[day]["periods"]:
                     start = i["start"]["hours"] * 60 + i["start"]["minutes"]
                     finish = i["finish"]["hours"] * 60 + i["finish"]["minutes"]
-                    if start < now.hour * 60 + now.minute < finish:
+                    if start <= now.hour * 60 + now.minute <= finish:
                         period = i
                         break
                 temp = period["temperature"]
-                current_temp = getTempC()
-                if temp > current_temp:
-                    activate_relay()
-                else:
-                    deactivate_relay()
+            current_temp = getTempC()
+            if temp > current_temp:
+                activate_relay()
+            else:
+                deactivate_relay()
             time.sleep(30)
 
 
@@ -63,25 +59,29 @@ def read():
     with open('week.json', 'r') as f:
         global week
         f.seek(0)
-        week = json.load(f)
+        with lock:
+            week = json.load(f)
 
 
-read()
-r = Regulator()
-r.start()
+try:
+    read()
+    r = Regulator()
+    r.start()
 
-server_socket = socket.socket(
-    socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('', 5001))
-# become a server socket
-server_socket.listen(1)
+    server_socket = socket.socket(
+        socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('', 5001))
+    # become a server socket
+    server_socket.listen(1)
 
-while 1:
-    # accept connections from outside
-    (client_socket, address) = server_socket.accept()
-    # now do something with the clientsocket
-    conn = Socket(client_socket)
-    # sock.connect('localhost', 5001)
-    if conn.receive() == 'azd':
-        read()
-        print(week)
+    while 1:
+        # accept connections from outside
+        (client_socket, address) = server_socket.accept()
+        # now do something with the clientsocket
+        conn = Socket(client_socket)
+        # sock.connect('localhost', 5001)
+        if conn.receive() == 'azd':
+            read()
+            print(week)
+finally:
+    GPIO.cleanup()
